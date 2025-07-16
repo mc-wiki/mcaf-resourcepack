b3eb82708143a0c7e02567803b62dfb407d0755d
violine1101 <violine1101@gmail.com>
2025-07-16 03:50:35 +0200

    wip: generating own progress bars

---
+++ update_progress.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+# TODO:
+# 1. log in to crowdin with (secret) API token
+#    API calls with headers:
+#        Authorization: Bearer <ACCESS_TOKEN>
+#        Accept: application/json
+# 2. api call to https://crowdin.com/api/v2/projects/777584/languages/progress
+# 3. generate images in this folder for each project
+# 4. could also use file-specific project via
+#    https://crowdin.com/api/v2/projects/777584/languages/fr/progress
+# Maybe: update text in README.md's
+
+print("TODO")
